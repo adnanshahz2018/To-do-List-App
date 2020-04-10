42491e87a884f4c4537e050363fe5d7ea1c0e1fe
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-04-10 10:24:31 +0500

    Created React App

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Learning React
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Adnan Shah here
         </a>
       </header>
     </div>
